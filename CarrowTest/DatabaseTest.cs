2f02d436b21ae9248faa0ad1ad317e7166e8028e
Yury Rivera <yrivera@applaudostudios.com>
2021-01-30 17:52:10 -0600

    :white_check_mark: more testing

--- CarrowTest/DatabaseTest.cs
+++ CarrowTest/DatabaseTest.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using Carrow;
 using Xunit;
 
@@ -15,6 +16,27 @@ namespace CarrowTest
             Assert.Equal("Juan", res[0]);
             Assert.Equal("Miguel", res[1]);
             Assert.Equal(count, res.Count);
+        }
+
+        [Fact]
+        public void ShouldTrowException()
+        {
+            var ex = Assert.Throws<DataException>(() => { new Database().ReadCustomers(7); });
+
+            Assert.Equal("cannot open DB connection", ex.Message);
+        }
+
+        [Theory]
+        [InlineData("Jose")]
+        [InlineData("Marco")]
+        public void ShouldInsertCustomers(string customer)
+        {
+            var db = new Database();
+            var users = db.ReadCustomers(6);
+            db.AddCustomer(customer);
+            var updated = db.ReadCustomers(7);
+            Assert.Equal(customer, updated[6]);
+            Assert.True(updated.Count > users.Count);
         }
     }
 }
