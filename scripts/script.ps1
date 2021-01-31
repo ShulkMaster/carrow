d55b8cd89ec8fbd4a35e528591b9353256516b11
Yury Rivera <yrivera@applaudostudios.com>
2021-01-31 14:48:22 -0600

    :rocket: migrating to powershell scripts 🤔😭

---
+++ scripts/script.ps1
@@ -0,0 +1,15 @@
+Write-Host "Downloading DocFX"
+Invoke-WebRequest https://github.com/dotnet/docfx/releases/download/v2.56.6/docfx.zip -o docfx.zip 
+Write-Host "Downloading template"
+Invoke-WebRequest https://github.com/jbltx/DiscordFX/archive/v0.3.zip -o template.zip
+mkdir ./Carrow/Docs/templates
+Write-Host "Extracting DocFx"
+Expand-Archive docfx.zip -DestinationPath ./docfx -Verbose
+Write-Host "Extracting Template"
+Expand-Archive template.zip -DestinationPath .\Carrow\Docs\templates -Verbose
+Move-Item .\Carrow\Docs\templates\DiscordFX-0.3\discordfx\* .\Carrow\Docs\templates\discordfx
+.\docfx\docfx.exe .\Carrow\Docs\docfx.json
+Write-Host "Compressing Site"
+Compress-Archive .\Carrow\Docs\_site\* -DestinationPath .\site.zip -CompressionLevel Optimal
+Write-Host "Moving to artifacg staging container"
+Move-Item .\site.zip $(Build.ArtifactStagingDirectory)/packs
